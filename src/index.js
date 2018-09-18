c26530577f5ba1da9e85796a14aca5433f456390
Ade-Joshe <femijay9@gmail.com>
2018-09-18 08:16:33 +0100

    added the jwt fucntion

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+//...other require statements
+const jwt = require('express-jwt');
+// const jwksRsa = require('jwks-rsa');
+
+
 // define the Express app
 const app = express();
 
@@ -32,60 +37,80 @@ app.use(morgan('combined'));
 
 // retrieve all questions
 app.get('/', (req, res) => {
-  const qs = questions.map(q => ({
-    id: q.id,
-    title: q.title,
-    description: q.description,
-    answers: q.answers.length,
-  }));
-  res.send(qs);
+	const qs = questions.map(q => ({
+		id: q.id,
+		title: q.title,
+		description: q.description,
+		answers: q.answers.length,
+	}));
+	res.send(qs);
 });
+
+// ... app.get endpoints ...
+
+const checkJwt = jwt({
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://qappapp.auth0.com/.well-known/jwks.json`
+	}),
+
+	// Validate the audience and the issuer.
+	audience: 'JfRfEcW6csbS9L5GzCIEC1431a0bE11i',
+	issuer: `https://qappapp.auth0.com/`,
+	algorithms: ['RS256']
+});
+
+// ... app.post endpoints ...
 
 
 
 // get a specific question
 app.get('/:id', (req, res) => {
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
-  res.send(question[0]);
+	const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+	if (question.length > 1) return res.status(500).send();
+	if (question.length === 0) return res.status(404).send();
+	res.send(question[0]);
 });
 
 
 
 // insert a new question
-app.post('/', (req, res) => {
-  const {title, description} = req.body;
-  const newQuestion = {
-    id: questions.length + 1,
-    title,
-    description,
-    answers: [],
-  };
-  questions.push(newQuestion);
-  res.status(200).send();
+app.post('/', checkJwt, (req, res) => {
+	const { title, description } = req.body;
+	const newQuestion = {
+		id: questions.length + 1,
+		title,
+		description,
+		answers: [],
+		author: req.user.name
+	};
+	questions.push(newQuestion);
+	res.status(200).send();
 });
 
 
 
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
-  const {answer} = req.body;
+app.post('/answer/:id', checkJwt, (req, res) => {
+	const { answer } = req.body;
 
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
+	const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+	if (question.length > 1) return res.status(500).send();
+	if (question.length === 0) return res.status(404).send();
 
-  question[0].answers.push({
-    answer,
-  });
+	question[0].answers.push({
+		answer,
+		author: req.user.name
+	});
 
-  res.status(200).send();
+	res.status(200).send();
 });
 
 
 
 // start the server
 app.listen(8081, () => {
-  console.log('listening on port 8081');
+	console.log('listening on port 8081');
 });
